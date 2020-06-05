9fdc58e5257b0eee84968d550b5ecd11e0501704
Sarav Shah <saravsshah@gmail.com>
2020-06-05 13:29:53 -0700

    Export auth0 context

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+export { default as Auth0Context } from './context/auth0-context';
 export { default as Auth0Provider } from './context/auth0-provider';
 export { default as useAuth } from './hooks/use-auth';
 export { default as withAuth } from './wrappers/with-auth';
